1087bee5a8964c2a10f6978f28a8d9ba63dcd0e3
engeto-student <66524373+engeto-student@users.noreply.github.com>
2020-07-10 16:44:49 +0200

    Update PROJEKT_2_s opakováním_čísel.py

--- PROJEKT_2_s opakováním_čísel.py
+++ PROJEKT_2_s opakováním_čísel.py
@@ -15,9 +15,6 @@ for i in range(0, n):
     cisla.append(cislo)
 print("generovaaná čísla (pro kontrolu funkčnosti programu): ", cisla)
 
-i = 0
-j = 0
-
 for j in range(0, n):
     tip = int(input("Tipni si číslo v mezích dolní a horní hranice intervalu: " ))
     tipy.append(tip)
@@ -27,29 +24,8 @@ for j in range(0, n):
                 print("Bull na pozici:", i)
                 Bulls_pocet = Bulls_pocet + 1
             else: print("Cows na pozici:", i); Cows_pocet = Cows_pocet + 1
+    if tipy[j] not in cisla:
+        print("Tipované číslo není mezi čísly generovanými.")
 
 print("Bulls: ", Bulls_pocet)
 print("Cows: ", Cows_pocet)
-
-'''Tvým úkolem bude vytvořit program, který by simuloval hru Bulls and Cows. Hra funguje následovně:
-
-Počítač vygeneruje tajné 4 místné číslo. Každá číslice tohoto čísla musí být jiná.
-Počítač vždy vyzve uživatele, aby hádal toto číslo.
-Počítač vyhodnotí tip uživatele a vrátí počty shod.
-Pokud uživatel uhádne správné číslo i správnou pozici, jedná se o "bulls". Pokud je pozice jiná, ale číslice je správná,
-jedná se o "cows".'''
-
-'''Příklad hry s číslem 2017
-Hi there!
-I've generated a random 4 digit number for you.
-Let's play a bulls and cows game.
-Enter a number
->>> 1234
-0 bulls, 2 cows
->>> 6147
-1 bull, 1 cow
->>> 2417
-3 bulls, 0 cows
->>> 2017
-Correct, you've guessed the right number in 4 guesses!
-That's {amazing, average, not so good, ...}'''
