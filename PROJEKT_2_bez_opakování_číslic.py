521ef59f9453ce96f713d2f72f22d2c1b9825614
engeto-student <66524373+engeto-student@users.noreply.github.com>
2020-07-12 08:21:45 +0200

    Update PROJEKT_2_bez_opakování_číslic.py

--- PROJEKT_2_bez_opakování_číslic.py
+++ PROJEKT_2_bez_opakování_číslic.py
@@ -1,41 +1,37 @@
 import random
-cisla = set()
-tipy = []
-seznam = []
+numbers = set()
+tips = []
+list = []
 
 Bulls_pocet = 0
 Cows_pocet = 0
-
-i = 0
-j = 0
 
 n = int(input("Vlož počet her: "))
 m = int(input("Vlož počet číslic: "))
 
 for i in range(0, n):
     for i in range(0, m):
-        cisla.clear()
-        while len(cisla) < m:
+        numbers.clear()
+        while len(numbers) < m:
             cislice = str(random.randint(1, 9))
-            cisla.add(cislice)
-    seznam.append(int("".join(cisla)))
+            numbers.add(cislice)
+    list.append(int("".join(numbers)))
 
-print("Seznam generovaných čísel (pro kontrolu algoritmu): ", seznam)
-
-i = 0
-j = 0
+print("Seznam generovaných čísel (pro kontrolu algoritmu): ", list)
 
 for j in range(0, n):
     tip = int(input("Tipni si číslo: " ))
-    tipy.append(tip)
+    tips.append(tip)
     for i in range(0, n):
-        if tipy[j] == seznam[i]:
+        if tips[j] == list[i]:
             if j == i:
                 print("Bull na pozici:", i)
                 Bulls_pocet = Bulls_pocet + 1
-            else: print("Cows na pozici:", i); Cows_pocet = Cows_pocet + 1
-    if tipy[j] not in cisla:
-        print("Tipované číslo není mezi čísly generovanými.")
+                print("Bulls: ", Bulls_pocet)
+                print("Cows: ", Cows_pocet)
+            else: print("Cows na pozici:", i); Cows_pocet = Cows_pocet + 1; print("Bulls: ", Bulls_pocet); print("Cows: ", Cows_pocet)
 
-print("Bulls: ", Bulls_pocet)
-print("Cows: ", Cows_pocet)
+    if tips[j] not in list:
+        print("Tipované číslo není mezi čísly generovanými.")
+        print("Bulls celkem: ", Bulls_pocet)
+        print("Cows celkem: ", Cows_pocet)
